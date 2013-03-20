9bb5e2a68f64ac3dc4ec09c32512fb28e2de05b7
Jökull Sólberg Auðunsson <jokull@solberg.is>
2013-03-20 22:57:20 +0000

    Initial commit ᶘ ᵒᴥᵒᶅ

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+"""Pythonic Setup for ssstatic."""
+
+
+import setuptools
+
+
+setuptools.setup(
+    version='0.1',
+    name='ssstatic',
+    description='Push a folder of assets to S3 with guaranteed cache busting.',
+    author=u'Jökull Sólberg Auðunsson',
+    author_email='jokull@solberg.is',
+    license='See LICENSE.',
+    url='https://github.com/jokull/ssstatic',
+    scripts=['ssstatic'],
+)
