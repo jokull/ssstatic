1e3c3c577e5ed8633ff3829a2d15b4a60bf69d33
Jökull Sólberg Auðunsson <jokull@solberg.is>
2013-04-09 13:45:45 +0000

    Version 0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 
 
 setuptools.setup(
-    version='0.1',
+    version='0.2',
     name='ssstatic',
     description='Push a folder of assets to S3 with guaranteed cache busting.',
     author=u'Jökull Sólberg Auðunsson',
